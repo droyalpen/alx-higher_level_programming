744e6ac7945b2afe26889be5a0e1cef4ca425cf5
droyalpen <droyalpen@gmail.com>
2022-12-05 06:00:44 -0800

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,19 +1,18 @@
 #include "lists.h"
 /**
- * is_palindrome - checks if palindrome
- * @head: head of node
- * Return: 0 if not, 1 if is
+ * is palindrome - checks if palindrome
+ * Return: 0 if not , 1 if is
  */
 int is_palindrome(listint_t **head)
 {
 	unsigned int len = 1;
 	listint_t *temp;
 
-	if (head == NULL|| *head == NULL)
+	if (head ==NULL|| *head == NULL)
 		return (1);
 
 	temp = *head;
-	while (temp) /* get len of list */
+	while (temp) /* get len of list*/
 	{
 		temp = temp->next;
 		len++;
