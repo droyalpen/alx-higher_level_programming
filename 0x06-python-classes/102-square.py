147cf1c12cf804f6a4b1a9b19fabb7cfbc0279df
droyalpen <droyalpen@gmail.com>
2022-12-20 04:00:13 -0800

    102-square.py

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,51 +1,58 @@
 
-
 #!/usr/bin/python3
-"""
-This is the "Square"  module.
-This module provides a simple Square class with initialize size.
-Defaults size to 0. Raise error on invalid size inputs.
-Methods Getter and Setter properties for size.
-Method area returns size of area of the square.
-Methods __lt__, __le__, __eq__, __ne__, __gt__, __ge__ allow
-comparisons between Square objects and their sizes.
+"""This module contains a class that defines a square.
+In the Square class we initialize each object by the
+__init__ method with a private instance variable called
+__size that takes the size variable's value passed as
+argument. Also checks if the size arg has a valid value.
 """
 
 
-class Square:
-    """A class that defines a square by size and can compute area"""
-    def __init__(self, size=0):
+class Square():
+    """A square class."""
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialization of the class square."""
         self.size = size
+        self.position = position
+
+    def area(self):
+        """The area of the Square"""
+        return self.__size ** 2
 
     @property
     def size(self):
+        """The size of the Square"""
         return self.__size
 
     @size.setter
-    def size(self, size):
-        if type(size) != int:
+    def size(self, SizeValue):
+        """set size of the Square"""
+        if type(SizeValue) != int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if SizeValue < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
-
-    def area(self):
-        return self.__size * self.__size
-
-    def __lt__(self, other):
-        return self.__size < other.size
-
-    def __le__(self, other):
-        return self.__size <= other.size
-
-    def __eq__(self, other):
-        return self.__size == other.size
-
-    def __ne__(self, other):
-        return self.__size != other.size
+        self.__size = SizeValue
 
     def __gt__(self, other):
-        return self.__size > other.size
+        """greater"""
+        return self.area() > other.area()
+
+    def __lt__(self, other):
+        """less"""
+        return self.area() < other.area()
+
+    def __eq__(self, other):
+        """equal"""
+        return self.area() == other.area()
 
     def __ge__(self, other):
-        return self.__size >= other.size
+        """greater than or equal"""
+        return self.area() >= other.area()
+
+    def __le__(self, other):
+        """less than or equal"""
+        return self.area() <= other.area()
+
+    def __ne__(self, other):
+        """not equal operator"""
+        return self.area() != other.area()
