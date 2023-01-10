5762f0d1c952debd90193496f7b5c1ac74f17b6d
droyalpen <93287030+droyalpen@users.noreply.github.com>
2023-01-10 17:43:36 +0100

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-"""Documentation for a load_from_json"""
-
+"""load_from_json_file
+"""
 import json
 
+
 def load_from_json_file(filename):
-    """Takes a JSON string and returns the object from a file
-    
-    Args: 
-        filename (str): the file to load the string from
-        
-    Returns: 
-        the JSON object represented by the string in the file
+    """Returns created object from a JSON file
     """
 
-    with open(filename) as f:
-        json_obj = json.load(f)
-    return json_obj
+    with open(filename, mode="r", encoding="UTF-8") as readFile:
+       return json.load(readFile)
