67fdfe8592b627b4a0da2df73fafbc6bbc1090f5
droyalpen <93287030+droyalpen@users.noreply.github.com>
2023-01-10 12:42:31 +0100

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Function add_attribute
-"""
+"""initializate"""
 
 
-def add_attribute(a_class, name, value):
-    """Adds new attribute to an object if it's possible
-    """
+class MyInt(int):
+    """class MyInt"""
 
-    # Set for O(1) membership test
-    cannot_add = {int, str, float, list, dict, tuple, frozenset, type, object}
+    def __ne__(self, next):
+        """negation to equal"""
+        return super().__eq__(next)
 
-    if type(a_class) in cannot_add:
-        raise TypeError("can't add new attribute")
-
-    a_class.__setattr__(name, value)
+    def __eq__(self, next):
+        """equal to negation"""
+        return super().__ne__(next)
