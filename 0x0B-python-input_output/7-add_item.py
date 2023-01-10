900e256b526a52a30c856183ad5262bd47b0dd20
droyalpen <93287030+droyalpen@users.noreply.github.com>
2023-01-10 17:47:24 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""Script continually adds arguments to a JSON string in a file
-    called add_item.json
-"""
-
 from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""access commandline arguments"""
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+"""create object from JSON file"""
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+"""writes an object to text file, using JSON representation"""
 
-if __name__ == "__main__":
-    try:
-        json_list = load_from_json_file("add_item.json")
-    
-    except Exception:
-        json_list = []
-    
-    i = 1
-    while (i < len(argv)):
-        json_list.append(argv[i])
-        i += 1
-    save_to_json_file(json_list, "add_item.json")
+filename = "add_item.json"
+try:
+    content = load_from_json_file(filename)
+except:
+    content = []
+
+for i in range(1, len(argv)):
+    content.append(argv[i])
+save_to_json_file(content, filename)
